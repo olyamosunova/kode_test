c226b379a3ab305b22d027def30e07392296d575
olyamosunova <vesnushka.ol@mail.ru>
2021-09-07 22:22:50 +0200

    change path to image

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import './App.css';
 import Authorization from '../Authorization/Authorization';
 import { AuthorizationStatus } from '../../const';
--- app/src/components/ErrorBlock/ErrorBlock.js
+++ app/src/components/ErrorBlock/ErrorBlock.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './ErrorBlock.css';
+import gif from './cry.gif';
 
 const ErrorBlock = ({ message }) => {
     return (
         <div className="error-block">
             <p>{ message }</p>
 
-            <img src="/images/cry.gif" alt="sticker" />
+            <img src={gif} alt="sticker" />
         </div>
     );
 };
