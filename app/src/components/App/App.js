691e4eafcd77f5ccc39d5b288050eef27bc94b37
olyamosunova <vesnushka.ol@mail.ru>
2021-09-09 12:22:31 +0200

    change router type

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Authorization from '../Authorization/Authorization';
 import CardDetail from '../CardDetail/CardDetail';
 import Cards from '../Cards/Cards';
@@ -49,7 +49,7 @@ const App = () => {
 
     return (
         <div className='app'>
-            <Router>
+            <Router basename='/kode_test'>
                 <Switch>
                     <Route
                         exact
